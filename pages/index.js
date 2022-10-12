25052825123328218e7196f6d08daaa8c3ca8451
KpWit <95849848+KpWit@users.noreply.github.com>
2022-10-12 20:27:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
 
       </section>
       
-      <section id="sec1" >
+      <section id="sec2" >
         <div className={styles.sec1}>
             <a href="https://shorturl.asia/jnJPB">
               <Image
