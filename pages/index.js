a91bdd46d63cd1737ee2ec89ce77aaf3d053b6a5
KpWit <95849848+KpWit@users.noreply.github.com>
2022-10-18 12:24:43 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,8 +42,8 @@ export default function Home() {
       <section id="sec1" >
         <div className={styles.sec1}>
               <Image
-                width={1280}
-                height={600}
+                width={1024}
+                height={480}
                 src="/034ng"
               />
         </div>
