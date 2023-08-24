d7bd27065c33d6e65393c4cd4d2a2ac3604e9a76
KpWit <95849848+KpWit@users.noreply.github.com>
2023-08-24 12:45:43 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-ใimport Head from 'next/head'
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/Nav.js'
 import Footer from '../components/Footer.js'
